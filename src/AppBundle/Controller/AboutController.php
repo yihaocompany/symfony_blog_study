c6ccdf45376ba051a97b18af3288cb350461e874
yihaocompany <574574@qq.com>
2017-02-05 14:21:29 +0800

    引入bootstrap模板
    引入bootstrap模板

---
+++ src/AppBundle/Controller/AboutController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class AboutController extends Controller
+{
+    /**
+     * @Route("/about", name="About")
+     */
+    public function indexAction(Request $request)
+    {
+        die('About controller');
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+}
